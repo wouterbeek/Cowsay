5520d8d25b5b66374f6bf3b65ac5badc73619d70
Wouter Beek <me@wouterbeek.com>
2014-08-21 19:41:14 +0200

    * [DEP] Run from command line.

--- run.pl
+++ run.pl
@@ -1,8 +1,21 @@
 % Standalone startup for the cowsay application.
 
+:- use_module(library(optparse)).
+
 :- if(current_prolog_flag(argv, ['--debug'])).
   :- ensure_loaded(debug).
 :- else.
   :- ensure_loaded(load).
 :- endif.
+
+:- use_module(cowsay(cowsay)).
+
+:- set_prolog_flag(verbose, silent).
+
+:- initialization(cowsay).
+
+cowsay:-
+  opt_arguments([], _, Message),
+  cowsay(Message),
+  halt.
 
